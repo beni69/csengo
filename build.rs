6fdf5a5c0496aa4564070b3c56a420172ddd5f63
beni69 <73666440+beni69@users.noreply.github.com>
2023-08-23 21:00:46 +0200

    frontend bundler business

---
+++ build.rs
@@ -0,0 +1,30 @@
+use std::{
+    io::{self, Write},
+    process::Command,
+};
+
+fn main() {
+    // for some weird reason ci builds fail on this step on windows
+    #[cfg(windows)]
+    if std::env::var("CI").is_ok() {
+        return;
+    }
+
+    println!("cargo:rerun-if-changed=frontend");
+
+    pnpm_exec("install");
+    pnpm_exec("build");
+}
+
+fn pnpm_exec(cmd: &str) {
+    println!("running: pnpm {cmd}");
+    let out = Command::new("pnpm")
+        .arg(cmd)
+        .current_dir("frontend")
+        .output()
+        .unwrap_or_else(|_| panic!("pnpm {cmd} failed"));
+
+    println!("status: {}", out.status);
+    io::stderr().write_all(&out.stdout).unwrap();
+    io::stderr().write_all(&out.stderr).unwrap();
+}
---
+++ frontend/bundle.js
@@ -0,0 +1,6 @@
+import "htmx.org";
+import "htmx.org/dist/ext/sse";
+
+import "@picocss/pico";
+
+import "./style.css";
